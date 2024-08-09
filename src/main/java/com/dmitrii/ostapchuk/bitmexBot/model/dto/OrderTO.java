13356a9e7ca90cc04a3b694f7bbf3cd5019cc988
Dmitrii Ostapchuk <dmit.ostapchuk@gmail.com>
2024-08-09 09:26:55 +0500

    Readme and description are completed

--- src/main/java/com/dmitrii/ostapchuk/bitmexBot/model/dto/OrderTO.java
+++ src/main/java/com/dmitrii/ostapchuk/bitmexBot/model/dto/OrderTO.java
@@ -1,15 +1,12 @@
 package com.dmitrii.ostapchuk.bitmexBot.model.dto;
 
 import com.dmitrii.ostapchuk.bitmexBot.model.OrderSide;
-import com.dmitrii.ostapchuk.bitmexBot.model.OrderStatus;
 import com.dmitrii.ostapchuk.bitmexBot.model.OrderType;
 import com.dmitrii.ostapchuk.bitmexBot.model.Symbol;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @Data
 @Builder
